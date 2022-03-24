0409f09be91e91e25831bef09b5fa559394943d8
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2022-03-24 10:40:30 +0900

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./styles.css";
+import { Header } from "./layouts/index";
 
 export default function App() {
   return (
     <div className="App">
+      <Header />
       <div className="main"></div>
     </div>
   );
